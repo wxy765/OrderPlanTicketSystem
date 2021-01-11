fd4409a3a2bf14b50807fd94b3cfb2f650384c89
Administrator <1913387695@qq.com>
2021-01-11 17:47:35 +0800

    飞机订票管理系统

--- src/Customer.java
+++ src/Customer.java
@@ -1,8 +1,12 @@
+import java.util.Date;
+
 public class Customer {
-    private String id;
-    private String name;
-    private String phone;
-    private String cardId;
+    private String id;//没有业务意义
+    private String customerType;//旅客类型：成人、儿童、婴儿
+    private String name;//旅客姓名
+    private String phone;//电话
+    private String cardId;//身份证
+    private Date birthDate;//出生日期
 
     public String getId() {
         return id;
@@ -10,6 +14,14 @@ public class Customer {
 
     public void setId(String id) {
         this.id = id;
+    }
+
+    public String getCustomerType() {
+        return customerType;
+    }
+
+    public void setCustomerType(String customerType) {
+        this.customerType = customerType;
     }
 
     public String getName() {
@@ -34,5 +46,13 @@ public class Customer {
 
     public void setCardId(String cardId) {
         this.cardId = cardId;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
     }
 }
